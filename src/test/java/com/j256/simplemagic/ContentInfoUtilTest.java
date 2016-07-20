06a8c2e2443ce48ec36337802e73c436c3b24a62
Gray Watson <gray.github@mailnull.com>
2016-07-20 01:30:52 -0400

    Removed broken test file.

--- src/test/java/com/j256/simplemagic/ContentInfoUtilTest.java
+++ src/test/java/com/j256/simplemagic/ContentInfoUtilTest.java
@@ -20,73 +20,66 @@ public class ContentInfoUtilTest {
 
 	private ContentInfoUtil contentInfoUtil;
 
-	private FileType[] fileTypes =
-			new FileType[] { //
-					//
-					new FileType("/files/x.gif", ContentType.GIF, "gif", "image/gif",
-							"GIF image data, version 89a, 32 x 32"),
-					new FileType("/files/x.pdf", ContentType.PDF, "pdf", "application/pdf", "PDF document, version 1.3"),
-					new FileType("/files/x.png", ContentType.PNG, "png", "image/png",
-							"PNG image, 205 x 189, 8-bit/color RGB, non-interlaced"),
-					new FileType("/files/x.tiff", ContentType.TIFF, "tiff", "image/tiff", "TIFF image data, big-endian"),
-					new FileType("/files/x.zip", ContentType.ZIP, "zip", "application/zip",
-							"Zip archive data, at least v1.0 to extract"),
-					new FileType("/files/x.javaserial", ContentType.OTHER, "Java", null,
-							"Java serialization data, version 5"),
-					new FileType("/files/x.doc", ContentType.MICROSOFT_WORD, "word", "application/msword",
-							"Microsoft Office Document Microsoft Word Document"),
-					new FileType("/files/x.docx", ContentType.MICROSOFT_WORD_XML, "word",
-							"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-							"Microsoft Word 2007+"),
-					new FileType("/files/x.rtf", ContentType.RTF, "rtf", "text/rtf",
-							"Rich Text Format data, version 1, unknown character set unknown version"),
-					new FileType("/files/x.xml", ContentType.XML, "xml", "application/xml", "XML document text"),
-					new FileType("/files/jfif.jpg", ContentType.JPEG, "jpeg", "image/jpeg",
-							"JPEG image data, JFIF standard 1.01"),
-					// partial file here
-					new FileType("/files/exif.jpg", ContentType.JPEG, "jpeg", "image/jpeg",
-							"JPEG image data, EXIF standard 2.1"),
-					new FileType("/files/exif2.jpg", ContentType.JPEG, "jpeg", "image/jpeg",
-							"JPEG image data, EXIF standard"),
-					new FileType("/files/x.jp2", ContentType.JPEG_2000, "jp2", "image/jp2", "JPEG 2000 image"),
-					new FileType("/files/x.class", ContentType.JAVA_APPLET, "applet", "application/x-java-applet",
-							"compiled Java class data, version 49.0"),
-					new FileType("/files/x.perl", ContentType.PERL, "perl", "text/x-perl",
-							"Perl script text executable"),
-					new FileType("/files/x.bz2", ContentType.BZIP2, "bzip2", "application/x-bzip2",
-							"bzip2 compressed data, block size = 900k"),
-					new FileType("/files/x.gz", ContentType.GZIP, "gzip", "application/x-gzip",
-							"gzip compressed data, from Unix, last modified: 2013-05-08 02:57:08 +0000"),
-					new FileType("/files/x.m4v", ContentType.MP4A, "mp4a", "video/mp4",
-							"ISO Media, MPEG v4 system, iTunes AVC-LC"),
-					new FileType("/files/x.xls", ContentType.OTHER, "OLE", null, "OLE 2 Compound Document"),
-					new FileType("/files/x.xlsx", ContentType.MICROSOFT_EXCEL_XML, "excel",
-							"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-							"Microsoft Excel 2007+"),
-					new FileType("/files/x.odt", ContentType.OPENDOCUMENT_TEXT, "opendocument-text",
-							"application/vnd.oasis.opendocument.text", "OpenDocument Text"),
-					new FileType("/files/x.html", ContentType.HTML, "html", "text/html", "HTML document text"),
-					new FileType("/files/x.aiff", ContentType.AIFF, "aiff", "audio/x-aiff", "IFF data, AIFF audio"),
-					new FileType("/files/x.mp3", ContentType.AUDIO_MPEG, "mpeg", "audio/mpeg",
-							"MPEG ADTS, layer III, v1, 128 kbps, 44.1 kHz, Stereo"),
-					new FileType("/files/x.wav", ContentType.WAV, "wav", "audio/x-wav",
-							"RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, stereo 44100 Hz"),
-					// NOTE: this seems to be somewhat valid since powerpoint uses this format
-					new FileType("/files/x.ppt", ContentType.OTHER, "OLE", null, "OLE 2 Compound Document"),
-					new FileType("/files/x.pptx", ContentType.MICROSOFT_POWERPOINT_XML, "powerpoint",
-							"application/vnd.openxmlformats-officedocument.presentationml.presentation",
-							"Microsoft PowerPoint 2007+"),
-					new FileType("/files/x.7z", ContentType.SEVEN_Z, "7zip", "application/x-7z-compressed",
-							"7-zip archive data, version 0.3"),
-					// truncated file
-					new FileType("/files/x.nuv", ContentType.OTHER, "MythTV", null,
-							"MythTV NuppelVideo v (640x480),progressive,aspect:1.00,fps:29.97"),
-					new FileType("/files/x.webp", ContentType.WEBP, "webp", "image/webp",
-							"RIFF (little-endian) data, WEBP image"),
-					new FileType("/files/x.exe", ContentType.OTHER, "MythTV", null,
-							"MythTV NuppelVideo v (640x480),progressive,aspect:1.00,fps:29.97"),
+	private FileType[] fileTypes = new FileType[] { //
+			//
+			new FileType("/files/x.gif", ContentType.GIF, "gif", "image/gif", "GIF image data, version 89a, 32 x 32"),
+			new FileType("/files/x.pdf", ContentType.PDF, "pdf", "application/pdf", "PDF document, version 1.3"),
+			new FileType("/files/x.png", ContentType.PNG, "png", "image/png",
+					"PNG image, 205 x 189, 8-bit/color RGB, non-interlaced"),
+			new FileType("/files/x.tiff", ContentType.TIFF, "tiff", "image/tiff", "TIFF image data, big-endian"),
+			new FileType("/files/x.zip", ContentType.ZIP, "zip", "application/zip",
+					"Zip archive data, at least v1.0 to extract"),
+			new FileType("/files/x.javaserial", ContentType.OTHER, "Java", null, "Java serialization data, version 5"),
+			new FileType("/files/x.doc", ContentType.MICROSOFT_WORD, "word", "application/msword",
+					"Microsoft Office Document Microsoft Word Document"),
+			new FileType("/files/x.docx", ContentType.MICROSOFT_WORD_XML, "word",
+					"application/vnd.openxmlformats-officedocument.wordprocessingml.document", "Microsoft Word 2007+"),
+			new FileType("/files/x.rtf", ContentType.RTF, "rtf", "text/rtf",
+					"Rich Text Format data, version 1, unknown character set unknown version"),
+			new FileType("/files/x.xml", ContentType.XML, "xml", "application/xml", "XML document text"),
+			new FileType("/files/jfif.jpg", ContentType.JPEG, "jpeg", "image/jpeg",
+					"JPEG image data, JFIF standard 1.01"),
+			// partial file here
+			new FileType("/files/exif.jpg", ContentType.JPEG, "jpeg", "image/jpeg",
+					"JPEG image data, EXIF standard 2.1"),
+			new FileType("/files/exif2.jpg", ContentType.JPEG, "jpeg", "image/jpeg", "JPEG image data, EXIF standard"),
+			new FileType("/files/x.jp2", ContentType.JPEG_2000, "jp2", "image/jp2", "JPEG 2000 image"),
+			new FileType("/files/x.class", ContentType.JAVA_APPLET, "applet", "application/x-java-applet",
+					"compiled Java class data, version 49.0"),
+			new FileType("/files/x.perl", ContentType.PERL, "perl", "text/x-perl", "Perl script text executable"),
+			new FileType("/files/x.bz2", ContentType.BZIP2, "bzip2", "application/x-bzip2",
+					"bzip2 compressed data, block size = 900k"),
+			new FileType("/files/x.gz", ContentType.GZIP, "gzip", "application/x-gzip",
+					"gzip compressed data, from Unix, last modified: 2013-05-08 02:57:08 +0000"),
+			new FileType("/files/x.m4v", ContentType.MP4A, "mp4a", "video/mp4",
+					"ISO Media, MPEG v4 system, iTunes AVC-LC"),
+			new FileType("/files/x.xls", ContentType.OTHER, "OLE", null, "OLE 2 Compound Document"),
+			new FileType("/files/x.xlsx", ContentType.MICROSOFT_EXCEL_XML, "excel",
+					"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Microsoft Excel 2007+"),
+			new FileType("/files/x.odt", ContentType.OPENDOCUMENT_TEXT, "opendocument-text",
+					"application/vnd.oasis.opendocument.text", "OpenDocument Text"),
+			new FileType("/files/x.html", ContentType.HTML, "html", "text/html", "HTML document text"),
+			new FileType("/files/x.aiff", ContentType.AIFF, "aiff", "audio/x-aiff", "IFF data, AIFF audio"),
+			new FileType("/files/x.mp3", ContentType.AUDIO_MPEG, "mpeg", "audio/mpeg",
+					"MPEG ADTS, layer III, v1, 128 kbps, 44.1 kHz, Stereo"),
+			new FileType("/files/x.wav", ContentType.WAV, "wav", "audio/x-wav",
+					"RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, stereo 44100 Hz"),
+			// NOTE: this seems to be somewhat valid since powerpoint uses this format
+			new FileType("/files/x.ppt", ContentType.OTHER, "OLE", null, "OLE 2 Compound Document"),
+			new FileType("/files/x.pptx", ContentType.MICROSOFT_POWERPOINT_XML, "powerpoint",
+					"application/vnd.openxmlformats-officedocument.presentationml.presentation",
+					"Microsoft PowerPoint 2007+"),
+			new FileType("/files/x.7z", ContentType.SEVEN_Z, "7zip", "application/x-7z-compressed",
+					"7-zip archive data, version 0.3"),
+			// truncated file
+			new FileType("/files/x.nuv", ContentType.OTHER, "MythTV", null,
+					"MythTV NuppelVideo v (640x480),progressive,aspect:1.00,fps:29.97"),
+			new FileType("/files/x.webp", ContentType.WEBP, "webp", "image/webp",
+					"RIFF (little-endian) data, WEBP image"),
+			// TODO: need to fix this
+			// new FileType("/files/x.exe", ContentType.OTHER, "Exe", null, "Exe"),
 			// end
-			};
+	};
 
 	@Test
 	public void testFiles() throws Exception {
@@ -231,6 +224,7 @@ public class ContentInfoUtilTest {
 		final String expectedName;
 		final String expectedMimeType;
 		final String expectedMessage;
+
 		private FileType(String fileName, ContentType expectedType, String expectedName, String expectedMimeType,
 				String description) {
 			this.fileName = fileName;
