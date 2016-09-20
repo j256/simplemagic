879bda39f4741db655c76f034988eede9ce0f9af
Gray Watson <gray.github@mailnull.com>
2016-09-20 14:19:19 -0400

    Added more tests for exe formats which exposed some pattern issues.

--- src/test/java/com/j256/simplemagic/ContentInfoUtilTest.java
+++ src/test/java/com/j256/simplemagic/ContentInfoUtilTest.java
@@ -20,6 +20,12 @@ public class ContentInfoUtilTest {
 
 	private ContentInfoUtil contentInfoUtil;
 
+	/**
+	 * File with their expected information.
+	 * 
+	 * NOTE: the file contents may be truncated since most of the magic number recognition happens at the front of the
+	 * file. Also for the executables, we don't want to publish runnable code to the outside world.
+	 */
 	private FileType[] fileTypes = new FileType[] { //
 			//
 			new FileType("/files/x.gif", ContentType.GIF, "gif", "image/gif", "GIF image data, version 89a, 32 x 32"),
@@ -71,14 +77,17 @@ public class ContentInfoUtilTest {
 					"Microsoft PowerPoint 2007+"),
 			new FileType("/files/x.7z", ContentType.SEVEN_Z, "7zip", "application/x-7z-compressed",
 					"7-zip archive data, version 0.3"),
-			// truncated file
 			new FileType("/files/x.nuv", ContentType.OTHER, "MythTV", null,
 					"MythTV NuppelVideo v (640x480),progressive,aspect:1.00,fps:29.97"),
 			new FileType("/files/x.webp", ContentType.WEBP, "webp", "image/webp",
 					"RIFF (little-endian) data, WEBP image"),
 			new FileType("/files/x.svg", ContentType.SVG, "svg", "image/svg+xml", "SVG Scalable Vector Graphics image"),
-			// TODO: need to fix this
-			// new FileType("/files/x.exe", ContentType.OTHER, "Exe", null, "Exe"),
+			new FileType("/files/windows.exe", ContentType.OTHER, "32", "application/x-dosexec",
+					"PE32 executable for MS Windows (GUI) Intel 80386 32-bit"),
+			new FileType("/files/dos.exe", ContentType.OTHER, "MS-DOS", "application/x-dosexec",
+					"MS-DOS executable, MZ for MS-DOS"),
+			new FileType("/files/dotnet.exe", ContentType.OTHER, "32", "application/x-dosexec",
+					"PE32 executable for MS Windows (GUI) Intel 80386 32-bit Mono/.Net assembly"),
 			// end
 	};
 
--- src/test/java/com/j256/simplemagic/types/IntegerTypeTest.java
+++ src/test/java/com/j256/simplemagic/types/IntegerTypeTest.java
@@ -7,25 +7,25 @@ import org.junit.Test;
 import com.j256.simplemagic.endian.EndianType;
 import com.j256.simplemagic.entries.MagicMatcher.MutableOffset;
 
-public class LongTypeTest {
+public class IntegerTypeTest {
 
 	@Test
 	public void testLittleEndianNumber() {
-		LongType longType = new LongType(EndianType.LITTLE);
-		String hexBytes = "0xc3cbc6c5c7b3a1";
+		IntegerType longType = new IntegerType(EndianType.LITTLE);
+		String hexBytes = "0x03cbc6c5";
 		Object testString = longType.convertTestString("lelong", hexBytes);
-		Object value = longType.extractValueFromBytes(0, new byte[] { hexToByte("0xa1"), hexToByte("0xb3"),
-				hexToByte("0xc7"), hexToByte("0xc5"), hexToByte("0xc6"), hexToByte("0xcb"), hexToByte("0xc3"), 0 });
+		Object value = longType.extractValueFromBytes(0,
+				new byte[] { hexToByte("0xc5"), hexToByte("0xc6"), hexToByte("0xcb"), hexToByte("0x03") });
 		assertNotNull(longType.isMatch(testString, null, false, value, new MutableOffset(0), null /* unused */));
 	}
 
 	@Test
 	public void testBigEndianNumber() {
-		LongType longType = new LongType(EndianType.BIG);
-		String hexBytes = "0xc3cbc6c5c7b3a1";
+		IntegerType longType = new IntegerType(EndianType.BIG);
+		String hexBytes = "0x03c7b3a1";
 		Object testString = longType.convertTestString("lelong", hexBytes);
-		Object value = longType.extractValueFromBytes(0, new byte[] { 0, hexToByte("0xc3"), hexToByte("0xcb"),
-				hexToByte("0xc6"), hexToByte("0xc5"), hexToByte("0xc7"), hexToByte("0xb3"), hexToByte("0xa1") });
+		Object value = longType.extractValueFromBytes(0,
+				new byte[] { hexToByte("0x03"), hexToByte("0xc7"), hexToByte("0xb3"), hexToByte("0xa1") });
 		assertNotNull(longType.isMatch(testString, null, false, value, new MutableOffset(0), null /* unused */));
 	}
 
