e7125c3dd1b42f51f261e5808d086dfca8fb2eba
Sergey Ponomarev <stokito@gmail.com>
2019-02-06 15:17:55 +0200

    MagicEntryTest: add asserts

--- src/test/java/com/j256/simplemagic/entries/MagicEntryTest.java
+++ src/test/java/com/j256/simplemagic/entries/MagicEntryTest.java
@@ -1,19 +1,20 @@
 package com.j256.simplemagic.entries;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class MagicEntryTest {
 
 	@Test
-	public void testCoverage() {
+	public void testToString() {
 		MagicEntry entry = new MagicEntry("name", 0, false, 0, null, null, null, false, 1, false, false, null);
-		entry.toString();
+		assertEquals("level 0,name 'name',test '1'", entry.toString());
 		MagicEntry entry2 = new MagicEntry(null, 0, false, 0, null, null, null, false, 1, false, false, null);
-		entry2.toString();
+		assertEquals("level 0,test '1'", entry2.toString());
 		MagicEntry entry3 = new MagicEntry(null, 0, false, 0, null, null, null, false, null, false, false,
 				new MagicFormatter("format"));
 		entry3.setMimeType("mime");
-		entry3.toString();
+		assertEquals("level 0,mime 'mime',format 'format'", entry3.toString());
 	}
 
 }
