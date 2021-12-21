af55c4fae94fd26f92b4b9af1d2382b0ed47643f
Gray Watson <gray.github@mailgw.com>
2021-12-20 21:45:06 -0500

    Removed

--- src/test/java/com/j256/simplemagic/logger/CommonsLoggingLogTest.java
+++
@@ -1,8 +0,0 @@
-package com.j256.simplemagic.logger;
-
-public class CommonsLoggingLogTest extends BaseLogTest {
-
-	public CommonsLoggingLogTest() {
-		super(new CommonsLoggingLog("CommonsLoggingLogTest"));
-	}
-}
--- src/test/java/com/j256/simplemagic/logger/Slf4jLoggingLogTest.java
+++
@@ -1,8 +0,0 @@
-package com.j256.simplemagic.logger;
-
-public class Slf4jLoggingLogTest extends BaseLogTest {
-
-	public Slf4jLoggingLogTest() {
-		super(new Slf4jLoggingLog("Slf4jLoggingLogTest"));
-	}
-}
